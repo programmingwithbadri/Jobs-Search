ced0905dbb17687a2fc420fc51a7a52afdaa3333
Badri Narayanan <programmingwithbadri@outlook.com>
2021-04-06 21:02:12 +0530

    Screens boilerplate

---
+++ src/screens/DeckScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const DeckScreen = () => {
+    return (
+        <View>
+            <Text> Screen boilerplate </Text>
+        </View>
+    )
+}
+
+export default DeckScreen
---
+++ src/screens/MapScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const MapScreen = () => {
+    return (
+        <View>
+            <Text> Screen boilerplate </Text>
+        </View>
+    )
+}
+
+export default MapScreen
