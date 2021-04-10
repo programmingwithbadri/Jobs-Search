57d379bed5f7f47c23a8ccabef41cf91d3b4774f
Badri Narayanan <programmingwithbadri@outlook.com>
2021-04-10 22:39:21 +0530

    Redux setup

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import Navigation from './src/config/Navigation';
+import { Provider } from 'react-redux'
+import store from './src/store';
 
-export default () => <Navigation />;
+export default () => <Provider store={store}> <Navigation /></Provider>;
