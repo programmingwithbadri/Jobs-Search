082f012f9590038908c73804f9819b7294cf2aca
Badri Narayanan <programmingwithbadri@outlook.com>
2021-04-13 11:31:08 +0530

    Set initial region for mapview

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { ActivityIndicator, View } from 'react-native';
 import MapView from 'react-native-maps'
 
 const MapScreen = () => {
+    useEffect(() => {
+        setMapLoaded(true)
+    });
+
+    const [region, setRegion] = useState({
+        longitude: -122,
+        latitude: 37,
+        longitudeDelta: 0.04,
+        latitudeDelta: 0.09
+    });
+
+    const [mapLoaded, setMapLoaded] = useState(false);
+
     return (
-        <View style={{ flex: 1 }}>
-            <MapView style={{ flex: 1 }} />
-        </View>
+        mapLoaded
+            ? <View style={{ flex: 1 }}>
+                <MapView region={region} style={{ flex: 1 }} />
+            </View>
+            : <View style={{ flex: 1, justifyContent: 'center' }}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
     )
 }
 
