13c8b4b9f7861e8e6a7e5136b0a54d221f86ba6c
Badri Narayanan <programmingwithbadri@outlook.com>
2021-04-14 09:59:16 +0530

    Use the mock data for getting jobs and navigate to deck screen

--- src/actions/jobActions.js
+++ src/actions/jobActions.js
@@ -1,35 +1,14 @@
 import axios from 'axios';
 import { FETCH_JOBS } from '../types';
-import reverseGeoCode from 'latlng-to-zip';
-import qs from 'qs';
-
-const JOB_QUERY_PARAMS = {
-    publisher: '4201738803816157',
-    format: 'json',
-    v: '2',
-    latlong: 1,
-    radius: 10,
-    q: 'javascript' // type of job
-}
-
-const JOBS_BASE_URL = 'http://api.indeed.com/ads/apisearch'
-
-const buildJobsUrl = (zipCode) => {
-    const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zipCode }) // location as l with zip code
-    return `${JOBS_BASE_URL}?${query}`
-}
+import data from '../data/jobs.json';
+import { navigate } from '../config/navigationRef';
 
 export const fetchJobs = (region) => async (dispatch) => {
-    try {
-        const zipCode = await reverseGeoCode(region);
-        const url = buildJobsUrl(zipCode)
-        const { data } = await axios.get(url);
-        dispatch({
-            type: FETCH_JOBS,
-            payload: data
-        });
-    }
-    catch (error) {
-        console.error(error);
-    }
+    console.log('data', data)
+    dispatch({
+        type: FETCH_JOBS,
+        payload: data
+    });
+
+    navigate('Deck')
 }
