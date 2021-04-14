db9c6811abd52f97b10fc9ecdd339bda067b792b
Badri Narayanan <programmingwithbadri@outlook.com>
2021-04-14 10:53:25 +0530

    get job results from redux state

--- src/actions/jobActions.js
+++ src/actions/jobActions.js
@@ -4,7 +4,6 @@ import data from '../data/jobs.json';
 import { navigate } from '../config/navigationRef';
 
 export const fetchJobs = (region) => async (dispatch) => {
-    console.log('data', data)
     dispatch({
         type: FETCH_JOBS,
         payload: data
--- src/screens/DeckScreen.js
+++ src/screens/DeckScreen.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux'
 import { Text, View } from 'react-native'
 
 const DeckScreen = () => {
+    const { jobs: { results } } = useSelector(state => state.jobs);
     return (
         <View>
             <Text> Deck Screen boilerplate </Text>
