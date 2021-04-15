39be005beed07cf70c6d788ee05f3d7237d00687
Badri Narayanan <programmingwithbadri@outlook.com>
2021-04-15 22:19:39 +0530

    Navigate to mapscreen button once no job found

--- src/screens/DeckScreen.js
+++ src/screens/DeckScreen.js
@@ -6,7 +6,7 @@ import MapView from 'react-native-maps'
 import { Button, Card } from 'react-native-elements';
 
 const renderCard = (job) => {
-    const initialRegion={
+    const initialRegion = {
         longitude: job.longitude,
         latitude: job.latitude,
         longitudeDelta: 0.02,
@@ -14,16 +14,18 @@ const renderCard = (job) => {
     };
 
     return (
-        <Card title={job.title}>
+        <Card>
+            <Card.Title>{job.title}</Card.Title>
             <View style={{ height: 300 }}>
                 <MapView
                     scrollEnabled={false}
                     style={{ flex: 1 }}
-                    cacheEnabled={Platform.OS==='android' ? true : false}
+                    cacheEnabled={Platform.OS === 'android' ? true : false}
                     initialRegion={initialRegion}
                 >
                 </MapView>
             </View>
+            <Card.Divider />
             <View style={styles.detailWrapper}>
                 <Text>{job.company}</Text>
                 <Text>{job.posted}</Text>
@@ -35,21 +37,31 @@ const renderCard = (job) => {
     )
 }
 
-const renderNoMoreCards = () => {
+const renderNoMoreCards = (cb) => {
     return (
-        <Card title="No more jobs">
+        <Card>
+            <Card.Title>No more jobs</Card.Title>
+            <Button
+                title="Back To Map"
+                large
+                icon={{ name: 'my-location' }}
+                backgroundColor="#03A9F4"
+                onPress={cb}
+            />
         </Card>
-    )
+    );
 }
 
-const DeckScreen = () => {
+const DeckScreen = ({ navigation }) => {
     const { jobs: { results } } = useSelector(state => state.jobs);
     return (
-        <View>
+        <View style={{ marginTop: 10 }}>
             <Swipe
                 data={results}
                 renderCard={renderCard}
-                renderNoMoreCards={renderNoMoreCards}
+                renderNoMoreCards={() => renderNoMoreCards(() => {
+                    navigation.navigate('Maps')
+                })}
             />
         </View>
     )
