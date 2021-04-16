4ff632cd78d1e1bcf29ea5645f333980ad04ced7
Badri Narayanan <programmingwithbadri@outlook.com>
2021-04-16 11:59:40 +0530

    Render the liked jobs in reviewScreen

--- src/screens/ReviewScreen.js
+++ src/screens/ReviewScreen.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Text, View } from 'react-native'
+import { Text, View, ScrollView, StyleSheet } from 'react-native'
+import { Card } from 'react-native-elements';
+
+const renderLikedJobs = (likedJobs) => {
+    return likedJobs.map(job => {
+        return (
+            <Card key={job.id}>
+                <View style={{ height: 200 }}>
+                    <View style={styles.detailWrapper}>
+                        <Text style={{ fontStyle: 'italic' }}>{job.company}</Text>
+                        <Text>{job.posted}</Text>
+                    </View>
+                </View>
+            </Card>
+        )
+    })
+}
 
 const ReviewScreen = () => {
     const likedJobs = useSelector(state => state.likedJobs)
     console.log(likedJobs)
     return (
-        <View>
-            <Text>Review Screen boilerplate </Text>
-        </View>
+        <ScrollView>
+            {renderLikedJobs(likedJobs)}
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    detailWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: 10
+    }
+})
 
 export default ReviewScreen
